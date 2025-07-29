bda23d75a5de0b43e40585ca7ca7465392dd8892
ananya045 <chaudharyananya0403@gmail.com>
2025-07-29 06:43:10 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 987654
+let accountEmail = "annu@gamil.com"
+var accountPassword = "12345"
+accountCity = "Udaipur"
+let accountState;
+
+//accountId = 98453//not allowed
+accountEmail = "ac@ac.in"
+accountPassword = 23456
+accountCity = "Dehradun"
+
+/*
+Prefer not to use var keyword
+because of issuue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
