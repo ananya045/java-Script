bd2437d5811c4b81b66e2d7a00cbda60bd0b0b13
ananya045 <chaudharyananya0403@gmail.com>
2025-07-29 12:23:02 +0000

    operations

--- 01_basics/03_conversionORoperation.js
+++ 01_basics/03_conversionORoperation.js
@@ -7,7 +7,7 @@ console.log(typeof valueINumber);
 console.log(valueINumber);
 
 let store = null
-console.log(store);
+//console.log(store);
 
 //"78" => 78
 //"78abc" => NaN
@@ -25,8 +25,54 @@ console.log(typeof booleanIsLoggedIn);
 
 let someNumber = 76
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**************************** Operations *********************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello!"
+let str2 = " Ananya"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2)
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
