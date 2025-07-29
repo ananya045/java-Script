525d661169846104a989edf6b1e7a233858787aa
ananya045 <chaudharyananya0403@gmail.com>
2025-07-29 07:11:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//treat all js code as newer version
+
+//alert("hello")//we are using nodejs ,not browser
+
+let name = "Ananya"//string
+let age = 20//Number
+let islogged = true//boolean 
+
+// number => 2 to power 53
+//bigint use in treading ,stock market facebook for large number 
+//string => "" may be in single or double qoutes
+//boolean => true or false
+//null = it is datatypes also but it is standalone value
+//undefined => variable declared but not given any value
+//symbol => uniqueness
+
+//object 
+console.log(typeof null);
+console.log(typeof undefined);
+
