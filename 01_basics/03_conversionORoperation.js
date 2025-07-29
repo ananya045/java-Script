e74163f1105374a4efde630c70a3e2cad19453ee
ananya045 <chaudharyananya0403@gmail.com>
2025-07-29 07:41:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionORoperation.js
@@ -0,0 +1,32 @@
+let score = "78abc"
+
+console.log(typeof score);
+
+let valueINumber = Number(score)
+console.log(typeof valueINumber);
+console.log(valueINumber);
+
+let store = null
+console.log(store);
+
+//"78" => 78
+//"78abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "Ananya" => true
+
+let someNumber = 76
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
