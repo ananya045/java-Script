3fe4caa6b3ba4fa54941b9ce7af9225cd6043854
ananya045 <chaudharyananya0403@gmail.com>
2025-07-29 13:13:03 +0000

    change in location of file

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,18 @@
+//  PRIMITIVE data types are call by value
+
+// 7 Types : String ,Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+
+// Array,Objects,Functions
+
+const friends = ['Nandini','Navya','Akshita'] //Array
+let myObj = {
+    name:"Ananya",
+    age: 20,
+} // object
+
+const myFunction = function () {
+    console.log("Hello! World");
+    
+}
