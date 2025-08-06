332d19e74648a976cc2977283edaaec6c151a578
ananya045 <chaudharyananya0403@gmail.com>
2025-08-06 14:34:09 +0000

    Local and Global scobe

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+let a = 10
+if(true){
+    let a = 10
+    const b = 20
+    //var c = 30
+    //console.log(a);
+     
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+function one(){
+    const username = "Ananya"
+    
+    function two() {
+        const website = " wikipedia " 
+        //console.log(username);
+               
+    }
+    //console.log(website);
+    //two()
+    
+}
+//one()
+
+if (true) {
+    const nam = "Annu "
+    if (nam === "Annu ") {
+        const web = "youtube"
+        //console.log(nam + web);
+    }
+
+    //console.log(web);
+    
+}
+//console.log(nam);
+
+function add(num){
+    return num+1
+}
+console.log(add(5))
+
+const addTwo = function(num) {
+    return num + 2
+}
+addTwo(5)
+
+
