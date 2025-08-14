66243ac58f8168a5d7b8074c9e01ff371c651df6
ananya045 <chaudharyananya0403@gmail.com>
2025-08-14 07:54:59 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS(IIFE)
+(function tea(){
+    console.log(`Db Connected`);
+    
+})();
+
+((name) => {
+    console.log(`DB CONNECTED ${name}`);
+    
+})("Anni")
