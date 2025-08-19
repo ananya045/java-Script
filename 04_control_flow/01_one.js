3cc08744bf7526b0d992185dfec62d512f811674
ananya045 <chaudharyananya0403@gmail.com>
2025-08-19 08:56:56 +0000

    control loop and switch

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -1,6 +1,56 @@
 //if
-const isUserloggedIn = true
+// const isUserloggedIn = true
 
-if( 2== "2"){
-    console.log("executed");
+// if( 2== "2"){
+//     console.log("executed");
+// }
+
+// const score = 200
+
+// if(score>100){
+//     let power = "fly"
+    //console.log(`User power:${power}`);
+
+    
+// }
+// console.log(`User power:${power}`)
+
+
+// const balance = 1000
+// if(balance<500) {
+//     console.log("less than 500");
+// } else if(balance<700) {
+//     console.log("less than 700");
+// } else if (balance<900){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1100");
+    
+// }
+
+
+
+// const userLoggedIn= true
+// const debitCard= true
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
 }
